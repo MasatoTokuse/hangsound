25774208cff3445a85fa10aa91ea7c4c1922d204
MasatoTokuse <masadinti@gmail.com>
2022-01-30 14:26:25 +0900

    impl ping pong

--- src/wsserver.rs
+++ src/wsserver.rs
@@ -9,12 +9,22 @@ impl Handler for Client {
         self.out.send("opened")
     }
 
-    fn on_message(&mut self, _: Message) -> Result<()> {
-        self.out.broadcast("play_music")
+    fn on_message(&mut self, message: Message) -> Result<()> {
+        let msg = match message.as_text() {
+            Ok(v) => v,
+            Err(e) => panic!("Problem message.as_text: {:?}", e),
+        };
+        if msg == "play" {
+            return self.out.broadcast("play_music");
+        }
+        if msg == "ping" {
+            return self.out.send("pong");
+        }
+        Ok(())
     }
 }
 
 fn main() {
     // 第二引数はラムダ
-    listen("127.0.0.1:3012", |out| Client { out: out } ).unwrap();
+    listen("127.0.0.1:3012", |out| Client { out: out }).unwrap();
 }
